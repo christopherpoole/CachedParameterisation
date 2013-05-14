2f27338a79318bef7c782203d7762a38cb6f4c2d
Christopher Poole <mail@christopherpoole.net>
2013-05-14 15:41:00 +1000

    user specifies number of records to generate

--- examples/basic/data.py
+++ examples/basic/data.py
@@ -1,9 +1,13 @@
+import sys
+
 import numpy
 import h5py
 
 
+count = int(sys.argv[1])
+
 f = h5py.File("data.hdf5")
-d = f.create_dataset('transform', (3, 10), int, chunks=(1, 2))
-d[...] = numpy.arange(30).reshape((3, 10))
+d = f.create_dataset('transform', (3, count), int, chunks=(1, 2))
+d[...] = numpy.random.random((3, count))*100
 f.close()
 
