61eb4512552709b90ead99afa3b62a374ed4c624
Christopher Poole <mail@christopherpoole.net>
2013-05-15 14:52:14 +1000

    builds more generic data in an array

--- data.py
+++ data.py
@@ -4,10 +4,25 @@ import numpy
 import h5py
 
 
-count = int(sys.argv[1])
+chunks = int(sys.argv[1])
+count = int(sys.argv[2])
+ndim = int(sys.argv[3])
+spacing = float(sys.argv[4])
+
+coords = zip(
+             *map(
+                lambda x: x.flatten(),
+                numpy.mgrid[
+                    numpy.repeat(
+                        slice(-count, count+1), ndim
+                    )
+                ] * spacing
+             )
+         )
 
 f = h5py.File("data.hdf5")
-d = f.create_dataset('transform', (3, count), int, chunks=(1, 2))
-d[...] = numpy.random.random((3, count))*100
+d = f.create_dataset('transform', (len(coords), ndim), int, chunks=(chunks, 1))
+#d[...] = numpy.random.random((3, count))*100
+d[...] = coords
 f.close()
 
